ba5cdf1dfa8617935c7b40482c111c92aebc8df4
Jacket777 <70706967+Jacket777@users.noreply.github.com>
2023-05-24 14:29:03 +0800

    控制结构

---
+++ C基础编程04_程序控制结构/code09_跳转基本使用.c
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<math.h>
+#include<time.h>
+
+int main() {
+	for (int i = 0; i < 10; i++) {
+		if (i == 5) {
+			break;
+		}
+		for (int j = 0; j < 10; j++) {
+			printf("%d %d\n", i, j);
+		}
+	}
+	return 0;
+}
+
+int main0102() {
+	for (int i = 0; i < 100; i++) {
+		if (i % 2 == 1) {
+			continue;
+		}
+		printf("%d\n", i);
+	}
+	return 0;
+}
